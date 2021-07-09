fb5c0a2343e508d044b0e0f26a7f250192893d23
bcombs <bob.combs@oracle.com>
2021-07-09 15:46:10 -0700

    add logging at rest requests

--- travelSalesSB/src/main/java/com/suse/travelsales/spring/api/TravelingSalesman.java
+++ travelSalesSB/src/main/java/com/suse/travelsales/spring/api/TravelingSalesman.java
@@ -7,25 +7,35 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 @RestController
 public class TravelingSalesman {
+
+    static Logger logger = Logger.getLogger(TravelingSalesman.class.getName());
+
     @PostMapping(path="/permutations")
     public List<List<City>> getPerumutations(@RequestBody List<City> cities) {
         TourCalculator calc = new TourCalculator(cities);
-        return calc.getPerumtations();
+        List<List<City>> result = calc.getPerumtations();
+        logger.info("Calculated " + result.size() + " permutations.");
+        return result;
     }
 
     @PostMapping(path="/distances")
     public List<List<Tour>> getDistances(@RequestBody List<City> cities) {
         TourCalculator calc = new TourCalculator(cities);
-        return calc.getSortedTours();
+        List<List<Tour>> tours = calc.getSortedTours();
+        logger.info("Calculated " + tours.size() + " unique sets of distances.");
+        return tours;
     }
 
     @PostMapping(path="/tour")
     public List<Tour> getShortest(@RequestBody List<City> cities) {
         TourCalculator calc = new TourCalculator(cities);
-        return calc.getShortest();
+        List<Tour> tours = calc.getShortest();
+        logger.info("Shortest distance has " + tours.size() + " unique paths.");
+        return tours;
     }
 
     @GetMapping(path="/random", produces = "application/json")
